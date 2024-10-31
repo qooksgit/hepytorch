64653ffbaad152f593bb86e710cd33b4ff35bed7
i550012 <wonqook.choi@sap.com>
2024-10-31 05:17:46 +0100

    chore: logging

--- hepytorch/hepytorch.py
+++ hepytorch/hepytorch.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import torch.cuda as cuda
 from .factories.model_factory import ModelFactory
 from .factories.dataloader_factory import DataLoaderFactory
@@ -15,16 +16,25 @@ __all__ = ("HEPTorch",)
 
 class HEPTorch:
     def __init__(self, config):
+        handle = "hepytorch"
+        logger = logging.getLogger(handle)
         self.device = "cuda" if cuda.is_available() else "cpu"
+        logger.info(f"Using {self.device} device")
         with open(config, "r") as f:
             self.config = json.load(f)
+        logger.info(
+            "Using the following configuration\n" + json.dumps(self.config, indent=2)
+        )
+
         dataloader = DataLoaderFactory().create_instance(self.config.get("data"))
         data = dataloader.load_data()
         self.preprocessor = PreprocessorFactory().create_instance(
             self.config.get("preprocessor")
         )
         self.data = self.preprocessor.data(data)
+        logger.info(f"Data shape after preprocessing: {self.data.shape}")
         self.target = self.preprocessor.target(data)
+        logger.info(f"Target shape after preprocessing: {self.target.shape}")
         self.model = ModelFactory().create_instance(self.config.get("model"))
         loss_fn = LossFnFactory().create_instance(self.config.get("loss_fn"))
         self.loss_fn = loss_fn.get_loss_fn()
